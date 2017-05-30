89a43ea8f5c1bf4dc3af632f011065b9a37b5e95
guruahn <guruahn@gmail.com>
2017-05-31 05:10:26 +0900

    connect reducer on Participants

--- src/participants/Participants.js
+++ src/participants/Participants.js
@@ -1,4 +1,12 @@
 import React, { Component, PropTypes } from 'react';
+import { database, firebaseAuth } from '../config/constants';
+import Loading from 'react-loading-animation'
+
+//Components
+
+//redux
+import { connect } from 'react-redux';
+import * as actions from './ParticipantsActions';
 
 class Participants extends Component {
     constructor(props) {
@@ -10,4 +18,23 @@ class Participants extends Component {
         );
     }
 }
-export default Participants;
+
+const mapStateToProps = (state) => {
+  return {
+    surveyDetail: state.participateReducer.surveyDetail,
+    surveyDetailQuerys: state.participateReducer.surveyDetailQuerys,
+    surveyDetailQuerysAnswers: state.participateReducer.surveyDetailQuerysAnswers,
+    loading: state.participateReducer.loading,
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleSetSurvey: (key, survey) => { dispatch(actions.setSurvey(key, survey)) },
+    handleSetLoading: (state) => { dispatch(actions.setLoading(state)) },
+    handleAddQueryAnswer: (queryKey, answer) => { dispatch(actions.addQueryAnswer(queryKey, answer)) },
+    handleSetQuerys: (querys) => { dispatch(actions.setQuerys(querys)) },
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Participants);
+export { Participants as PureParticipants};
